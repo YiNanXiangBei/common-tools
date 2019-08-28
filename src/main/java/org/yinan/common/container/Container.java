9cf0076a7de220c27023fcbbe8fd25923f5a41a9
yinan <demoshuwang@gmail.com>
2019-08-28 22:12:38 +0800

    拆分并初始化公共工具类

---
+++ src/main/java/org/yinan/common/container/Container.java
@@ -0,0 +1,19 @@
+package org.yinan.common.container;
+
+/**
+ * @author yinan
+ * @date 19-6-9
+ */
+public interface Container {
+
+    /**
+     * 接口容器启动方法
+     */
+    void start();
+
+    /**
+     * 接口容器停止方法
+     */
+    void stop();
+
+}
